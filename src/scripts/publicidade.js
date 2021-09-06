eb687d26e2ab32cd88e0bda6d3e29de7dd746cbe
Miyuki-08 <68870724+Miyuki-08@users.noreply.github.com>
2021-09-06 16:49:41 -0300

    Add files via upload

---
+++ src/scripts/publicidade.js
@@ -0,0 +1,20 @@
+const lista = [
+    {
+      id: 'https://sass-lang.com/',
+      src: '../images/descubra.webp',
+      alt: 'Imagem 1',
+      link: 'https://www.youtube.com/watch?v=povY6y1AXD0z'
+    },
+    {
+      id: 'https://sass-lang.com/',
+      src: '../images/descubra.webp',
+      alt: '',
+      link: 'caminho/do/arquivo'
+    },
+    {
+      id: '3',
+      src: '../images/descubra.png',
+      alt: '',
+      link: 'caminho/do/arquivo'
+    }
+];
